a826768cb6e4873f7028301aa0d486dd419042e2
Nash Vogeltanz <nashvogeltanz@gmail.com>
2026-08-01 20:06:20 -0400

    fix(spm): declare iOS platform on package (Combine availability for iOS builds)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PlummetKinematics",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v17)],
     targets: [
         .target(
             name: "PlummetKinematics",
